4ccc26339112b07322a530da3aea2998246ae6ac
Christine Wilks <crissxross@gmail.com>
2016-03-05 16:11:48 +0000

    add home & scene components & configured router

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,18 @@
 import {Component} from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+import {HomeComponent} from './home/home.component';
+import {SceneComponent} from './scene/scene.component';
+
 
 @Component({
-    selector: 'de-app',
-    template: '<h1>Dialogue Engine</h1>'
+  selector: 'de-app',
+  templateUrl: 'app/app.component.html',
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@RouteConfig([
+  { path: '/home', name: 'Home', component: HomeComponent },
+  { path: '/scene', name: 'Scene', component: SceneComponent }
+])
 export class AppComponent { }
---
+++ app/scene/scene.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+  selector: 'scene',
+  templateUrl: 'app/scene/scene.component.html'
+})
+
+export class SceneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+}
