c27a475cc8da2ad6c4d2298e62b3c15752b6bd27
Christine Wilks <crissxross@gmail.com>
2016-03-05 19:01:35 +0000

    minor edits

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from 'angular2/core';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
   selector: 'home',
   templateUrl: 'app/home/home.component.html',
-  styleUrls: ['app/home/home.component.css']
+  styleUrls: ['app/home/home.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 
 export class HomeComponent implements OnInit {
