d40c7c1124c4022779044bfe7a75f540466a74c2
Christine Wilks <crissxross@gmail.com>
2016-03-09 13:08:41 +0000

    add tslint.json

--- app/experiment/exp.component.ts
+++ app/experiment/exp.component.ts
@@ -1,12 +1,12 @@
-import {Component, OnInit} from "angular2/core";
+import {Component, OnInit} from 'angular2/core';
 
-import {DialogService} from "../services/dialog.service";
-import {Api} from "../services/api";
+import {DialogService} from '../services/dialog.service';
+import {Api} from '../services/api';
 
 @Component({
-  selector: "experiment",
-  templateUrl: "app/experiment/exp.component.html",
-  styleUrls: ["app/experiment/exp.component.css"]
+  selector: 'experiment',
+  templateUrl: 'app/experiment/exp.component.html',
+  styleUrls: ['app/experiment/exp.component.css']
 })
 
 export class ExperimentComponent implements OnInit {
