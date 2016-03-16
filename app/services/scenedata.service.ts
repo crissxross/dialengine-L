a6412d14cd52c13666e57d91675694c930ec7d38
Christine Wilks <crissxross@gmail.com>
2016-03-16 10:43:59 +0000

    add simple-arrays-dialog.json to fix player/actor components (broken after previous refactor)

--- app/services/scenedata.service.ts
+++ app/services/scenedata.service.ts
@@ -9,6 +9,7 @@ export class SceneDataService {
   private _sceneUrl = '../../content/mock-data/mock-dialog.json';
   private _simpleUrl = '../../content/mock-data/simple-dialog.json';
   private _simpleDataUrl = '../../content/mock-data/simple-dialog-data.json';
+  private _simpleArraysUrl = '../../content/mock-data/simple-arrays-dialog.json';
 
   constructor(private _http: Http) { }
 
@@ -41,19 +42,19 @@ export class SceneDataService {
       .catch(this.handleError);
   }
 
-  // getActorSimpleDialog() {
-  //   return this._http.get(this._simpleUrl)
-  //     .map((res: Response) => res.json().actor)
-  //     .do(actor => console.log(actor))
-  //     .catch(this.handleError);
-  // }
+  getActorSimpleDialog() {
+    return this._http.get(this._simpleArraysUrl)
+      .map((res: Response) => res.json().actor)
+      .do(actor => console.log(actor))
+      .catch(this.handleError);
+  }
 
-  // getPlayerSimpleDialog() {
-  //   return this._http.get(this._simpleUrl)
-  //     .map((res: Response) => res.json().player)
-  //     .do(player => console.log(player))
-  //     .catch(this.handleError);
-  // }
+  getPlayerSimpleDialog() {
+    return this._http.get(this._simpleArraysUrl)
+      .map((res: Response) => res.json().player)
+      .do(player => console.log(player))
+      .catch(this.handleError);
+  }
 
   private handleError(error: Response) {
     console.error(error);
